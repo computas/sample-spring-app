b2d1f4b942691bd66dd8fc7129659d39247f0219
Michael Gfeller <mgfeller@mgfeller.net>
2016-05-07 20:39:57 +0200

    Initial commit

---
+++ src/main/java/com/example/pipeline/PipelineWorkshopApplication.java
@@ -0,0 +1,12 @@
+package com.example.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineWorkshopApplication.class, args);
+	}
+}
